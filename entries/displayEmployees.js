e6e632e657db7e42d46e4ffc56bac2ceb965171e
amc9732 <amc9732@rit.edu>
2016-10-23 18:34:12 -0400

    quick fix for clearing the display table when hitting refresh

--- entries/displayEmployees.js
+++ entries/displayEmployees.js
@@ -6,7 +6,18 @@
  *
  */
 
- $(function(e) {
+// Quick fix to delete old entries when hitting refresh button.
+ var displayTableHtml = "<tbody><tr><th>Name</th><th>Email</th><th>Title</th><th>Department</th><th>Salary</th><th>Superiors</th><th>Subordinates</th><th>Status</th></tr></tbody>";
+
+ $(function () {
+    displayEmployees();
+ });
+
+ $("#displayEmployee").click(function () {
+     displayEmployees();
+  });
+
+function displayEmployees(){
     $.ajax({
         dataType: 'json',
         type: "GET",
@@ -14,18 +25,19 @@
         context: this,
         success: function (json){
             var tableContent = '';
+            $('#displayTable').html(displayTableHtml);
 
             $.each(json, function(key, value){
                 tableContent += '<tr>'
                 tableContent += '<td>' + value.fullName + '</td>';
                 tableContent += '<td>' + value.email + '</td>';
                 tableContent += '<td>' + value.title + '</td>';
-				tableContent += '<td>' + value.department + '</td>';
-				tableContent += '<td>' + value.salary + '</td>';
-				tableContent += '<td>' + value.superiors + '</td>';
-				tableContent += '<td>' + value.subordinates + '</td>';
-				tableContent += '<td>' + value.stat + '</td>';
-				tableContent += '</tr>'
+                tableContent += '<td>' + value.department + '</td>';
+                tableContent += '<td>' + value.salary + '</td>';
+                tableContent += '<td>' + value.superiors + '</td>';
+                tableContent += '<td>' + value.subordinates + '</td>';
+                tableContent += '<td>' + value.stat + '</td>';
+                tableContent += '</tr>'
             });
 
             $('#displayTable').append(tableContent);
@@ -35,4 +47,4 @@
         }
 
      });
- })
+ }
